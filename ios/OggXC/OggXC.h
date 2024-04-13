00394b0eadafe8c35b98a7881ad0f264afb34ddb
Hamilton <pwnified@harmonicdog.com>
2024-04-12 20:00:36 -0700

    XCFramework

---
+++ ios/OggXC/OggXC.h
@@ -0,0 +1,18 @@
+//
+//  OggXC.h
+//  OggXC
+//
+//  Created by Hami on 4/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OggXC.
+FOUNDATION_EXPORT double OggXCVersionNumber;
+
+//! Project version string for OggXC.
+FOUNDATION_EXPORT const unsigned char OggXCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OggXC/PublicHeader.h>
+
+
